82b78f8ab2f0dacd4992cbee179d4c1acf390224
Hack Reactor Student <student@hackreactor.com>
2017-04-19 21:04:37 -0700

    Completed some amazing things

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
-
+var http = require('http');
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
  * Consider using the `paths` object below to store frequently used file paths. This way,
  * if you move any files, you'll only need to change your code in one place! Feel free to
  * customize it in any way you wish.
  */
+
+
 
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
@@ -26,16 +28,111 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+
+	fs.readFile( (archive.paths.list), function(err, list) {
+	  
+	  if (err){ console.log("LIST WASNT FOUND"); }
+	  
+	  var splitList = list.toString().split(',');
+
+	  console.log("readListOfUrls CALLBACK",callback);
+	  if ( calllback ) { callback(splitList); }
+	  else { return splitList; }
+
+  });
+
 };
 
 exports.isUrlInList = function(url, callback) {
+
+	var list = this.readListOfUrls();
+
+	if ( !list.includes(url) ){
+		if ( callback ){ 
+			callback(false);
+		} else { 
+			return false; 
+		}
+	} else {
+		if ( callback ){ 
+			callback(true);
+		} else { 
+			return true; 
+		}
+	}
+
 };
 
 exports.addUrlToList = function(url, callback) {
+
+	var list = this.readListOfUrls();
+
+	var newFileContent = list.length === 0 ? url : (list+","+url);
+	// add url to list
+  fs.writeFile(archive.paths.list, newFileContent, 
+  	function(err) { if (err) throw err;
+	  console.log('ADDED URL: '+url );
+	});
+
 };
 
 exports.isUrlArchived = function(url, callback) {
+
+	fs.readFile( 
+		// check in archivedSites
+		(archive.paths.archivedSites+url), 
+		function(err, archivedFile) {
+
+		  if (err) { 
+		  	// false
+		  	if ( callback ){ 
+					callback(false);
+				} else { 
+					return false; 
+				}
+
+			} else {
+				if ( callback ){ 
+					callback(true);
+				} else { 
+					return true; 
+				}
+			}
+		  
+	});
+
 };
 
-exports.downloadUrls = function(urls) {
+exports.downloadUrls = function(urls){
+
+	console.log("Calling downloadUrls")
+	// go through each
+	urls.forEach(function(url){
+		// get request?
+		console.log("URL: ",url);
+		// http.get(url, function(response){
+		// 	console.log("RESPONSE: ", Object.keys(response))
+		// 	console.log("RESPONSE DOMAIN: ", response.client)
+		// })	
+
+		var options = {
+		  host: url,
+		  port: 80,
+		  path: '/index.html'
+		};
+
+		http.get(options, function(res) {
+		  console.log("Got response: " + res.statusCode);
+		}).on('error', function(e) {
+		  console.log("Got error: " + e.message);
+		});
+
+		// write to a new file
+	})
+
+		// visit each page
+		// copying to new file
+		// save that file in our archives, name of file is url
+		// remove from list 
+
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,120 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var qs = require('querystring');
 // require more modules/folders here!
 
+archive.downloadUrls(['www.google.com']);
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+	console.log( "\n\nREQ URL:",req.url );
+	console.log( "REQ METHOD:",req.method );
+
+	
+	if ( req.method === "POST"){
+
+		var serveFile;
+
+			// serve index
+			if ( req.url === '/' ){
+				fs.readFile( (archive.paths.siteAssets+'/index.html'), function(err, data) {
+				  if (err) throw err;
+				  serveFile = data;
+				  // res.end(data);
+				});
+			} 
+					
+			// serve rest of assets
+			var dataUrl;
+
+			req.on('data', function(chunk){
+				dataUrl = qs.parse(chunk.toString()).url;
+			}); 
+
+
+			req.on('end', function() {
+
+				console.log("DATA FROM POST", dataUrl);
+
+				fs.readFile( 
+					// check in archivedSites
+					(archive.paths.archivedSites+dataUrl), 
+					function(err, archivedFile) {
+
+						// if not in archivedSites
+					  if (err) {
+					  	// get the list
+					  	fs.readFile( (archive.paths.list), function(err, list) {
+								  if (err){ console.log("LIST WASNT FOUND"); }
+								  
+								  // CHECK IN LIST IF IT INCLUDES DATAURL
+								  var splitList = list.toString().split(',');
+
+								  if ( !splitList.includes(dataUrl) ){
+
+								  	var newFileContent = list.length === 0 ? dataUrl : (list+","+dataUrl);
+								  	// add url to list
+									  fs.writeFile(archive.paths.list, newFileContent, 
+									  	function(err) { if (err) throw err;
+										  console.log('ADDED URL: '+dataUrl,list.toString());
+										});
+
+								  } else {
+								  	console.log("LIST HAD URL: "+dataUrl)
+								  }
+								  
+								  // we alrways respond with loading.html
+								  fs.readFile( (archive.paths.siteAssets+'/loading.html'), function(err, loadingFile) {
+										  // if (err) throw err;
+										  if (err) console.log("ERROR SENDING LOADING FILE",err);
+										  // console.log("Serving to loading: ",loadingFile.toString('ascii'));
+										  serveFile = loadingFile.toString('ascii');
+										  res.end(serveFile);
+									});
+
+							});
+					  } else {
+						  // if site IS in archives
+						  // respond with page in archives
+						  // console.log("Serving to archived: ",archivedFile.toString('ascii'));
+						  serveFile = archivedFile.toString('ascii');
+						  res.end(serveFile);
+					  }
+
+				});
+
+			});
+
+			// console.log("FINAL SERVE", serveFile)
+			// res.end(serveFile);
+			
+	}
+
+
+	else if ( req.method === "GET"){
+
+			// serve index
+			if ( req.url === '/' ){
+
+					fs.readFile( 
+						// url
+						(archive.paths.siteAssets+'/index.html'), 
+						// callback
+						function(err, data) {
+						  if (err) throw err;
+						  res.end(data);
+						});
+			}
+			// serve style
+			else if ( req.url === '/styles.css' ){
+
+					fs.readFile( ( archive.paths.siteAssets + req.url ), function(err, data) {
+					  if (err) throw err;
+					  res.end(data);
+					});
+			}
+	}
+
+			
+
 };
